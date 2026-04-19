9383721c8d33978461a7925b3036423d3773b874
martins0023 <martinsmiracle45@gmail.com>
2026-04-19 18:47:14 +0100

    update

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -8,7 +8,6 @@ const router = useRouter()
 return (
     <div className="w-full flex fixed top-0 z-20 bg-white justify-between items-center p-3 font-montserrat">
       <button
-        to="/"
         className="flex m-1 items-center"
         onClick={() => router.back()}
       >
